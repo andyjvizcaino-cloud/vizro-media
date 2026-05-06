b93e4a0fa77fb5e02df619e02c139876850f98dc
Andy Vizcaino <andyvizcaino@Andys-MacBook-Pro.local>
2026-05-06 16:50:00 -0500

    Initial commit: Vizro Media marketing site

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/health') {
+        res.writeHead(200);
+        res.end('ok');
+        return;
+    }
+
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Server error');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        res.end(data);
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Vizro Media running on port ${PORT}`);
+});
